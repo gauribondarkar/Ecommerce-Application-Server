291bd74f80a83d45687268e33871833af7004f49
gauri_bondarkar <gauribo17@gmail.com>
2024-01-08 21:12:43 +0530

    updated

--- JsonConnect.js
+++ JsonConnect.js
@@ -1,13 +1,14 @@
 const dbConnect = require('./dbConnect');
 const Product = require('./models/Product');
 const Category = require('./models/Category');
+const dotenv = require('dotenv').config('./env')
 
 
 const productJson = require("./product.json");
 const categoryJson = require("./category.json");
 const start = async () => {
     try {
-        await dbConnect("mongodb://0.0.0.0/Ecommerce");
+        await dbConnect(process.env.MONGOURI);
         await Product.deleteMany();
         await Product.create(productJson);
 
--- dbConnect.js
+++ dbConnect.js
@@ -1,31 +1,16 @@
-// const mongoose = require('mongoose');
-
-// module.exports = async() =>{
-//     const mongoUri = "mongodb+srv://gauribo17:8sKaV1hdY3e7SIO6@cluster0.xocruzr.mongodb.net/?retryWrites=true&w=majority";
-//     try {
-//         const connect = await mongoose.connect(mongoUri);
-
-//         console.log(`mongodb connected: ${connect.connection.host}`)
-//     } catch (error) {
-//         console.log(error);
-//         process.exit(1);
-//     }
-    
-// }
-
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config('./env');
 
-module.exports = () => {
+module.exports = async() =>{
+    const mongoUri = process.env.MONGOURI;
+    try {
+        const connect = await mongoose.connect(mongoUri);
+
+        console.log(`mongodb connected: ${connect.connection.host}`)
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
     
-    mongoose.connect("mongodb://0.0.0.0/Ecommerce");
-    const conn = mongoose.connection;
-
-    conn.once('open', () => {
-        console.log("connected...");
-    })
-
-    conn.on('error', () => {
-        console.log("error");
-        process.exit();
-    })
 }
+
